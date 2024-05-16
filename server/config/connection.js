596e5f86e2f9b74af72641f92f6c5be83b1b08ef
JLP-55 <jack.v.pekin@gmail.com>
2024-05-16 21:09:07 +1000

    deployment message

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const {connect, connection} = require("mongoose");
 
-// this process.env is set in heroku, not local .env file
+// use process.env.MONGODB_URI (understood by heroku) or the local database
 connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/memory_gameDB");
 
 module.exports = connection;
